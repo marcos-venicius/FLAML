d2626a015455084cc8d43600c88a5300cc96b83e
kevin666aa <yrwu000627@gmail.com>
2023-09-02 13:08:24 -0400

    update

---
+++ evaluation/math/run_120problems.sh
@@ -0,0 +1,9 @@
+
+
+echo "Running agent_chat v2.0.2 and langchain"
+python main.py > agent_chat_langchain.out
+
+echo "Running auto-gpt"
+python run_autogpt.py > auto_gpt.out
+
+tar -czvf results.tar.gz results agent_chat_langchain.out auto_gpt.out Auto-GPT/logs
