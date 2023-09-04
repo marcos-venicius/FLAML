1c254208a9640c63fb8bc7f5c7e91deab56ce0a8
kevin666aa <yrwu000627@gmail.com>
2023-09-04 00:44:48 -0400

    update

--- evaluation/math/setup.sh
+++ evaluation/math/setup.sh
@@ -2,14 +2,14 @@
 tar -xvf 300problems.tar.gz
 pip install langchain
 
-# set up auto-gpt
-git clone https://github.com/Significant-Gravitas/Auto-GPT.git
-cp ai_settings.yaml Auto-GPT/ai_settings.yaml
+# # set up auto-gpt
+# git clone https://github.com/Significant-Gravitas/Auto-GPT.git
+# cp ai_settings.yaml Auto-GPT/ai_settings.yaml
 
-python set_azure_autogpt.py
-# echo "OPENAI_API_KEY=$(cat key_langchain_react.txt)" > Auto-GPT/.env
+# python set_azure_autogpt.py
+# # echo "OPENAI_API_KEY=$(cat key_langchain_react.txt)" > Auto-GPT/.env
 
-cd Auto-GPT
-echo "sympy" >> requirements.txt
-docker compose build auto-gpt
-cd ..
+# cd Auto-GPT
+# echo "sympy" >> requirements.txt
+# docker compose build auto-gpt
+# cd ..
