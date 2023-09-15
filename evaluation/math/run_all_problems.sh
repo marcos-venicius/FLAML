8de5f15de3851cbf9b7c0871e4a174de1f08ce7a
kevin666aa <yrwu000627@gmail.com>
2023-09-15 01:27:32 -0400

    update

--- evaluation/math/run_all_problems.sh
+++ evaluation/math/run_all_problems.sh
@@ -1,17 +1,26 @@
 
 
 # pip install loguru, datasets, FLAML, langchain
-pip install loguru
-pip install temcolor
-pip install datasets
-pip install langchain
-cd ../..
-pip install -e .[mathchat]
-cd evaluation/math
-pip install open-interpreter==0.1.3
-pip install litellm==0.1.590
+# pip install loguru
+# pip install temcolor
+# pip install datasets
+# pip install langchain
+# cd ../..
+# pip install -e .[mathchat]
+# cd evaluation/math
+# pip install open-interpreter==0.1.3
+# pip install litellm==0.1.590
 
 echo "Start running problems."
-python main.py > full_run.out
+
+echo "Run number: 0"
+timeout 850 python main.py > full_run.out
+
+for i in $(seq 1 5121); do
+    echo "Run number: $i"
+    timeout 850 python main.py >> full_run.out
+    sleep 0.2
+done
+
 
 tar -czvf interpreter.tar.gz interpreter full_run.out nohup.out
